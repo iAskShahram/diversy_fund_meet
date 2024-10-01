7118da09ba5d88e41b94627dcaaf5aee0c3b32f1
Ahmed Shahram <ahmed.shahram@ripeseed.io>
2024-10-01 14:55:16 +0500

    update: update suth.ts file

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,18 +1,40 @@
 import { env } from "@/env";
-import NextAuth, { type User, type DefaultSession } from "next-auth";
+import { signInSchema } from "@/lib/validators/auth";
+import { verifyPassword } from "@/utils/auth.util";
+import type { Role } from "@prisma/client";
+import createHttpError from "http-errors";
+import NextAuth, { type DefaultSession } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { db } from "./db";
 
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
-      id: string;
-      role: string;
+      id: number;
+      role: Role;
+      email: string;
+      image: string;
+      createdAt: Date;
+      updatedAt: Date;
     } & DefaultSession["user"];
   }
 
   interface User {
-    id?: string;
-    role?: string;
+    role: Role;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+}
+
+declare module "@auth/core/jwt" {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    id: number;
+    role: Role;
+    email: string;
+    image: string;
+    createdAt: Date;
+    updatedAt: Date;
   }
 }
 
@@ -38,28 +60,44 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         email: { label: "Email", type: "text" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials, req) {
-        // This function is already present and necessary
-        // You should implement your actual authentication logic here
-        // For example:
-        // const user = await db.user.findUnique({
-        //   where: {
-        //     email: credentials.username,
-        //   },
-        // });
-        // if (user && await comparePasswords(credentials.password, user.password)) {
-        //   return user;
-        // }
-        // return null;
+      async authorize(credentials) {
+        // validate email and password through schema
+        const values = await signInSchema.safeParseAsync({
+          email: credentials.email,
+          password: credentials.password,
+        });
+        if (!values.success) {
+          throw createHttpError.BadRequest(
+            values.error.errors[0]?.message ?? "Invalid email or password",
+          );
+        }
+        const user = await db.user.findUnique({
+          where: {
+            email: credentials.email as string,
+          },
+        });
 
-        // Placeholder return for demonstration:
-        const tmpuser: User = {
-          id: "1",
-          name: "J Smith",
-          email: "jsmith@example.com",
-          role: "admin",
+        if (!user) {
+          throw createHttpError.NotFound("User not found");
+        }
+        const isPasswordValid = await verifyPassword(
+          values.data.password,
+          user.password,
+        );
+        if (!isPasswordValid) {
+          throw createHttpError.Unauthorized("Invalid email or password");
+        }
+
+        // return user;
+
+        return {
+          id: user.id.toString(),
+          role: user.role,
+          email: user.email,
+          image: user.image,
+          createdAt: user.createdAt,
+          updatedAt: user.updatedAt,
         };
-        return tmpuser;
       },
     }),
   ],
@@ -68,12 +106,14 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     signIn: async ({ user, account, profile, email, credentials }) => {
       // log and check which one is executed in which order
       console.log("signIn :: ", user, account, profile, email, credentials);
+      // user ha values returned from authorzie
       return true;
     },
     jwt: async ({ token, user, account, profile, session, trigger }) => {
       console.log("jwt :: ", token, user, account, profile, session, trigger);
-      if (user) {
-        token.id = user.id;
+      // user ahs values returned from singin
+      if (user.id) {
+        token.id = parseInt(user.id);
         token.role = user.role;
       }
       return token;
