41bef83e8db7623cdc1c9d7ee11b02b5499e4dca
Ahmed Shahram <ahmed.shahram@ripeseed.io>
2024-10-02 11:22:28 +0500

    chore: add theme

--- src/components/ui/providers/providers.tsx
+++ src/components/ui/providers/providers.tsx
@@ -2,12 +2,17 @@ import { TRPCReactProvider } from "@/trpc/react";
 import { HydrateClient } from "@/trpc/server";
 import React from "react";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./theme-provider";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <TRPCReactProvider>
-        <HydrateClient>{children}</HydrateClient>
+        <HydrateClient>
+          <ThemeProvider attribute="class" defaultTheme="light">
+            {children}
+          </ThemeProvider>
+        </HydrateClient>
       </TRPCReactProvider>
       <Toaster />
     </>
