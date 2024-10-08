e540d51cc24834cad5be2c08fd280506bd7b8f60
iAskShahram <shahram.ask.546@gmail.com>
2024-10-08 22:47:56 +0500

    run fix

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons"
-import { DayPicker } from "react-day-picker"
+import { CustomComponents, DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
@@ -59,10 +59,10 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={{
-        IconLeft: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
-      }}
+      // components={{
+      //   IconLeft: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" {...props} />,
+      //   IconRight: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" {...props} />,
+      // } as CustomComponents}
       {...props}
     />
   )
