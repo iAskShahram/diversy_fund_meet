aeab700ce010aa417581ab0a0c43c7cf481e48c4
iAskShahram <shahram.ask.546@gmail.com>
2024-10-11 02:35:40 +0500

    feaT: share index page

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,15 +1,23 @@
-import { AdminSettings } from "./_components/AdminSettings";
-import { Logo } from "./_components/Logo";
-import { MainNav } from "./_components/MainNav";
+import { auth } from "@/server/auth";
+import { Announcements } from "./admin/_components/announcements";
+import { StatCards } from "./admin/_components/stat-cards";
+import { UpcomingMeetings } from "./admin/_components/upcoming-meetings";
+import { Role } from "@prisma/client";
 
-const Page = () => {
+const Page = async () => {
+  const session = await auth();
   return (
-    <div className="border-b">
-      <div className="flex h-16 items-center px-4">
-        <Logo />
-        <MainNav className="mx-6" />
-        <div className="ml-auto flex items-center space-x-4">
-          <AdminSettings />
+    <div className="flex h-full flex-col p-8 pt-6">
+      <div className="flex h-full flex-col gap-4">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-bold">{session?.user.role !== Role.USER && `Admin `}Dashboard</h1>
+        </div>
+        <div>
+          <StatCards />
+        </div>
+        <div className="flex h-full gap-6">
+          <UpcomingMeetings className="w-1/2" />
+          <Announcements className="w-1/2" session={session} />
         </div>
       </div>
     </div>
