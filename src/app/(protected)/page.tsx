42c3ecc1f846db99878c2545f4d7cbb75896a769
iAskShahram <shahram.ask.546@gmail.com>
2024-10-19 02:26:31 +0500

    chore: use helper then static checking

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -2,15 +2,18 @@ import { auth } from "@/server/auth";
 import { Announcements } from "./admin/_components/announcements";
 import { StatCards } from "./admin/_components/stat-cards";
 import { UpcomingMeetings } from "./admin/_components/upcoming-meetings";
-import { Role } from "@prisma/client";
+import { isAdmin } from "@/utils/auth.util";
+import { Session } from "next-auth";
 
 const Page = async () => {
-  const session = await auth();
+  const session = (await auth()) as Session;
   return (
     <div className="flex h-full flex-col p-8 pt-6">
       <div className="flex h-full flex-col gap-4">
         <div className="flex justify-between">
-          <h1 className="text-2xl font-bold">{session?.user.role !== Role.USER && `Admin `}Dashboard</h1>
+          <h1 className="text-2xl font-bold">
+            {isAdmin(session) && `Admin `}Dashboard
+          </h1>
         </div>
         <div>
           <StatCards />
