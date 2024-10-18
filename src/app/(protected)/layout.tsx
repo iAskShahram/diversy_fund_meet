28d1dc5dbdde1e0f6a0b0651731ed52b98c44d3a
iAskShahram <shahram.ask.546@gmail.com>
2024-10-19 03:07:06 +0500

    pass session to nav

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -3,17 +3,18 @@ import { Logo } from "./_components/Logo";
 import { Nav } from "./admin/_components/admin-nav";
 import { AdminSettings } from "./admin/_components/admin-settings";
 import { auth } from "@/server/auth";
+import { Session } from "@auth/core/types";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
-  const session = await auth();
+  const session = (await auth()) as Session;
   return (
     <div className="flex flex-col min-h-screen">
       <div className="h-16 border-b">
         <div className="flex h-16 items-center px-8">
           <Logo />
-          <Nav className="mx-16" />
+          <Nav className="mx-16" session={session} />
           <div className="ml-auto flex items-center space-x-4">
-            <AdminSettings session={session!} />
+            <AdminSettings session={session} />
           </div>
         </div>
       </div>
