f9ed80cf3290d509a2bfc4f21df8ace126bfbade
iAskShahram <shahram.ask.546@gmail.com>
2024-10-24 17:25:13 +0500

    feat: use react-hook-form to show realtime errors

--- src/app/auth/signin/_components/login-form.tsx
+++ src/app/auth/signin/_components/login-form.tsx
@@ -1,12 +1,22 @@
 "use client";
 
 import { SubmitButton } from "@/app/_components/submit-button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useFormState } from "react-dom";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { signInAction } from "../actions/singin";
 
 const initialState = {
@@ -14,38 +24,82 @@ const initialState = {
   success: false,
 };
 
+const formSchema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters" }),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
+
 export function LoginForm() {
   const [state, formAction] = useFormState(signInAction, initialState);
   const router = useRouter();
+
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
   useEffect(() => {
     if (state?.success) {
       router.push("/", { scroll: false });
     }
   }, [state, router]);
+  function onSubmit(values: FormSchema) {
+    const formData = new FormData();
+    formData.append("email", values.email);
+    formData.append("password", values.password);
+    formAction(formData);
+  }
 
   return (
-    <form action={formAction}>
-      <div className="grid gap-4">
-        <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
-          <Input
-            id="email"
-            name="email"
-            type="email"
-            placeholder="m@example.com"
-            required
-          />
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="m@example.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="grid gap-2">
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <SubmitButton txt="Login" />
         </div>
-        <div className="grid gap-2">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" name="password" type="password" required />
-        </div>
-        <SubmitButton txt="Login" />
-      </div>
-      <p role="status" className={cn(!!state.errors && "text-sm text-red-500")}>
-        {!state.success && state.errors.split(".")[0]}
-      </p>
-    </form>
+        <p
+          role="status"
+          className={cn(!!state.errors && "text-sm text-red-500")}
+        >
+          {!state.success && state.errors.split(".")[0]}
+        </p>
+      </form>
+    </Form>
   );
 }
